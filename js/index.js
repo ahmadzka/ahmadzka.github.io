5e95d2095ac3d6555454fb44e04d8163ef391cbd
ahmad adzka <144531135+ahmadzka@users.noreply.github.com>
2024-04-16 14:05:52 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+(function() {
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  var card = $('card'),
+      openB = $('open'),
+      closeB = $('close'),
+      nextL = $('next'),
+      timer = null;
+  console.log('wat', card);
+  var audio = new Audio('lagu.mp3');
+  openB.addEventListener('click', function () {
+    card.setAttribute('class', 'open-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', 'open-fully');
+      timer = null;
+    }, 1000);
+    audio.play();
+  });
+
+  closeB.addEventListener('click', function () {
+    card.setAttribute('class', 'close-half');
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', '');
+      timer = null;
+    }, 1000);
+  });
+
+}());
